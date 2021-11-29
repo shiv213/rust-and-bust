12253fe88f05527c11141c84b295217591e25357
Shiv Trivedi <shiv.v.trivedi@gmail.com>
2021-11-29 17:53:19 -0600

    sensor data being sent over serial port when requested and dependency issues resolved

--- jeshture/src/main.rs
+++ jeshture/src/main.rs
@@ -7,13 +7,13 @@
 use mpu6050::*;
 
 // The trait used by formatting macros like write! and writeln!
-use core::fmt::Write as FmtWrite;
+// use core::fmt::Write as FmtWrite;
 
 // The macro for our start-up function
 use cortex_m_rt::entry;
 
 // I2C HAL traits & Types.
-use embedded_hal::blocking::i2c::{Operation, Read, Transactional, Write};
+// use embedded_hal::blocking::i2c::{Operation, Read, Transactional, Write};
 
 // GPIO traits
 use embedded_hal::digital::v2::OutputPin;
@@ -42,6 +42,16 @@ use usb_device::{class_prelude::*, prelude::*};
 // USB Communications Class Device support
 use usbd_serial::SerialPort;
 
+// Serde support
+// use serde::{Serialize, Deserialize};
+
+// TODO define custom struct for IMU data
+// #[derive(Serialize, Deserialize)]
+// struct IMUData {
+//     accel_x: f32,
+//     accel_y: f32,
+//     accel_z: f32,
+// }
 
 //// The linker will place this boot block at the start of our program image. We
 //// need this to help the ROM bootloader get our code up and running.
@@ -107,15 +117,15 @@ fn main() -> ! {
 
     // Create a USB device with a fake VID and PID
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .manufacturer("Fake company")
-        .product("Serial port")
-        .serial_number("TEST")
+        .manufacturer("Rust and Bust")
+        .product("Jeshture")
+        .serial_number("128H")
         .device_class(2) // from: https://www.usb.org/defined-class-codes
         .build();
 
     let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
 
-    let mut i2c_pio = i2c_pio::I2C::<_, _, _, _, hal::gpio::FunctionPio0>::new(
+    let i2c_pio = i2c_pio::I2C::<_, _, _, _, hal::gpio::FunctionPio0>::new(
         &mut pio,
         pins.gpio20,
         pins.gpio21,
@@ -125,25 +135,31 @@ fn main() -> ! {
     );
 
     let mut mpu = Mpu6050::new(i2c_pio);
-
-    mpu.init(&mut delay);
+    // mpu.init(&mut delay).ok();
+    match mpu.init(&mut delay) {
+        Ok(_) => {
+            let _ = serial.write(b"MPU6050 init OK");
+        }
+        Err(_e) => {
+            let _ = serial.write(b"MPU6050 init error:");
+        }
+    }
 
     let mut led_pin = pins.led.into_push_pull_output();
 
-    mpu.setup_motion_detection();
+    let _ = mpu.setup_motion_detection();
 
     // let timer = hal::timer::Timer::new(pac.TIMER, &mut pac.RESETS);
 
     loop {
         if mpu.get_motion_detected().unwrap() {
             led_pin.set_high().unwrap();
-            let acc = mpu.get_acc().unwrap();
+            // let _ = serial.write(b"MOTION\n");
             // println!("r/p: {:?}", acc);
-            // let _ = serial.write(b"MOTION");
             // let temp_i16 = i16::from_be_bytes(acc.x as u8) >> 5;
             // let temp_f32 = f32::from(acc.x) * 0.125;
             // let _ = writeln!(serial, "Temperature: {:0.2}°C", temp_f32);
-            let _ = serial.write(&acc.x.to_bits().to_be_bytes());
+            // let _ = serial.write(&acc.x.to_bits().to_be_bytes());
         } else {
             led_pin.set_low().unwrap();
         }
@@ -164,10 +180,7 @@ fn main() -> ! {
         // let acc = mpu.get_acc()?;
         // println!("acc: {:?}", acc);
 
-        // led_pin.set_high().unwrap();
-        // delay.delay_ms(500);
-        // led_pin.set_low().unwrap();
-        // delay.delay_ms(500);
+        // delay.delay_ms(1000);
 
         if usb_dev.poll(&mut [&mut serial]) {
             let mut buf = [0u8; 64];
@@ -178,18 +191,29 @@ fn main() -> ! {
                 Ok(0) => {
                     // Do nothing
                 }
-                Ok(count) => {
-                    // Convert to upper case
-                    buf.iter_mut().take(count).for_each(|b| {
-                        b.make_ascii_uppercase();
-                    });
-                    // Send back to the host
-                    let mut wr_ptr = &buf[..count];
-                    while !wr_ptr.is_empty() {
-                        let _ = serial.write(wr_ptr).map(|len| {
-                            wr_ptr = &wr_ptr[len..];
-                        });
-                    }
+                Ok(_count) => {
+                    // // Convert to upper case
+                    // buf.iter_mut().take(count).for_each(|b| {
+                    //     b.make_ascii_uppercase();
+                    // });
+                    // // Send back to the host
+                    // let mut wr_ptr = &buf[..count];
+                    // while !wr_ptr.is_empty() {
+                    //     let _ = serial.write(wr_ptr).map(|len| {
+                    //         wr_ptr = &wr_ptr[len..];
+                    //     });
+                    // }
+                    let acc = mpu.get_acc().unwrap();
+                    let x = serde_json_core::to_string::<f32, 32>(&(*&acc.x as f32)).unwrap();
+                    let y = serde_json_core::to_string::<f32, 32>(&(*&acc.y as f32)).unwrap();
+                    let z = serde_json_core::to_string::<f32, 32>(&(*&acc.z as f32)).unwrap();
+                    let _ = serial.write(b"{ x: ");
+                    let _ = serial.write(x.as_bytes());
+                    let _ = serial.write(b", { y: ");
+                    let _ = serial.write(y.as_bytes());
+                    let _ = serial.write(b", { z: ");
+                    let _ = serial.write(z.as_bytes());
+                    let _ = serial.write(b"}\r\n");
                 }
             }
         }
