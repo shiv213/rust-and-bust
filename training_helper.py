0a3855ae5f78fbee84f52acef71523aa8a061d17
Jessica Singh <jessicasingh@outlook.com>
2021-12-02 16:48:08 -0600

    Make list of objects with accel and gyro values

--- training_helper.py
+++ training_helper.py
@@ -1,12 +1,41 @@
 import serial
 import json
 
+CONST_THRESHOLD = 5
+
 with serial.Serial() as ser:
+    aX = 100
+    aY = 100
+    aZ = 100
     ser.baudrate = 9600
-    ser.port = '/dev/ttyACM0'
+    print("1")
+    ser.port = 'COM5'
+    print("2")
     ser.open()
-    ser.write(b'0')
-    raw = ser.readline().decode('utf-8')
-    print(json.loads(raw))
-    ser.close()
+    print("3")
+    items = []
+    while(abs(aX) > CONST_THRESHOLD or abs(aY) > CONST_THRESHOLD or abs(aZ) > CONST_THRESHOLD):
+        ser.write(b'0')
+        print("4")
+        raw = ser.readline()
+        raw = raw.decode('utf-8')
+        print(type(raw))
+        # stringlist = raw.split("|")
+        dictionary = {}
+        dict(x.split('=') for x in raw.split('|'))
+        for x in raw.split('|'):
+            key, value = x.split('=')
+            value = float(value.strip())
+            dictionary[key.strip()] = value
+            # print(key)
+            # print(value)
+        items.append(dictionary)
+        print(raw)
+        print(dictionary)
+        # raw = raw.decode('utf-8')
+        # print(json.loads(dictionary))
+        aX = dictionary["aX"]
+        aY = dictionary["aY"]
+        aZ = dictionary["aZ"]
+ser.close()
 
